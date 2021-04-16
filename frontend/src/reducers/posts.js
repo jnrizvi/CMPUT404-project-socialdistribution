eb9926e694a3c3c6c5c7150eae80b1e15682f41d
Ryan Kang <rkang2009@gmail.com>
2021-04-15 19:53:54 -0600

    pagination in inbox

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -87,7 +87,7 @@ export default function(state = initialState, action) {
                         [itemId]: itemData
                     }
                 }
-            }
+            }    
         default:
             return state;
     }
