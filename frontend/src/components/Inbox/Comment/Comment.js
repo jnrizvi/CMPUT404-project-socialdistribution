8d1de5c1c0e8c85a4281a7a70acd62b130a5ef1e
Ryan Kang <rkang2009@gmail.com>
2021-04-16 19:36:42 -0600

    comments now have author name

--- frontend/src/components/Inbox/Comment/Comment.js
+++ frontend/src/components/Inbox/Comment/Comment.js
@@ -11,11 +11,13 @@ const useStyles = makeStyles(() => ({
         marginBottom: '40px',
         borderRadius: "8px",
         padding: '1em 2em',
-        display: 'flex'
     },
     summary: {
         marginTop: 'auto',
         marginBottom: 'auto'
+    },
+    author: {
+        fontSize: '0.75em'
     }
 }));  
 
@@ -24,6 +26,7 @@ export default function Comment(props) {
     const classes = useStyles();
     return (
         <div className={classes.root}>
+            <p className={classes.author}>Comment by: {props.data.author.displayName}</p>
             <p className={classes.summary}>{ props.data.comment }</p>
         </div>
     )
